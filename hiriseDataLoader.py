dbdf53fe3cfe4a9d5f4385c1dd2bb91f4816aca5
Scott McMichael <scott.t.mcmichael@nasa.gov>
2014-06-27 18:45:36 -0700

    Added initial files

---
+++ hiriseDataLoader.py
@@ -0,0 +1,337 @@
+#!/usr/bin/env python
+# __BEGIN_LICENSE__
+#  Copyright (c) 2009-2013, United States Government as represented by the
+#  Administrator of the National Aeronautics and Space Administration. All
+#  rights reserved.
+#
+#  The NGT platform is licensed under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance with the
+#  License. You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# __END_LICENSE__
+
+import sys
+
+from BeautifulSoup import BeautifulSoup
+
+import os, glob, optparse, re, shutil, subprocess, string, time, urllib, urllib2
+
+import multiprocessing
+
+import GoogleMapsEngine
+
+
+def man(option, opt, value, parser):
+    print >>sys.stderr, parser.usage
+    print >>sys.stderr, ''' Script for grabbing HiRISIE data files'''
+
+    sys.exit()
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+#--------------------------------------------------------------------------------
+
+# fileType is the file name after the prefix
+def generatePdsPath(filePrefix, fileType):
+    """Generate the full PDS path for a given HiRISE data file"""
+    
+    # File prefix looks like this: PSP_009716_1755 or ESP_011984_1755
+    
+    # Extract the mission code (only a few possibilities)
+    missionCode = filePrefix[0:3]
+    
+    # Determine which ORB folder this will be in (each contains 100 files)
+    # - Looks like this: ORB_001300_001399
+    frameNumber = filePrefix[4:9]
+    orbDir = 'ORB_'+ frameNumber[0:4] +'00_'+ frameNumber[0:4] +'99'
+    
+    filename = filePrefix + fileType
+    baseUrl  = "http://hirise-pds.lpl.arizona.edu/PDS/RDR/"
+    fullUrl  = baseUrl + missionCode +"/"+ orbDir +"/"+ filePrefix +"/"+ filename
+
+    print filePrefix + fileType + ' -> ' + fullUrl
+    return fullUrl
+
+# missionCode should be ESP or PSP
+def getDataList(outputFilePath, missionCode):
+    """Populates a text file list of all available HiRISE RDR data"""
+       
+    print 'Updating HiRISE PDS data list...'
+       
+    #http://hirise-pds.lpl.arizona.edu/PDS/RDR/<PSP or ESP>/<ORB_PATH>/<PSP or ESP PATH>/<FILE_NAME.JP2>
+    #http://hirise-pds.lpl.arizona.edu/PDS/RDR/ESP/ORB_011900_011999/ESP_011984_1755/
+    #http://hirise-pds.lpl.arizona.edu/download/PDS/RDR/PSP/ORB_001400_001499/PSP_001430_1815/PSP_001430_1815_RED.JP2
+    
+   
+    # TODO: Do another loop for ESP!
+    baseUrl = "http://hirise-pds.lpl.arizona.edu/PDS/RDR/"+missionCode+"/"
+
+    # Parse the top PDS level
+    parsedIndexPage = BeautifulSoup(urllib2.urlopen((baseUrl)).read())
+
+    outputFile = open(outputFilePath, 'w')
+
+    # Loop through outermost directory
+    for line in parsedIndexPage.findAll('a'):
+        orbName = line.string
+        if not 'ORB' in orbName: # Skip link up a directory
+            continue
+        orbPath = baseUrl + orbName
+        
+        #print 'Scanning directory ' + orbPath
+        
+        # Parse next directory level
+        orbPage = BeautifulSoup(urllib2.urlopen((orbPath)).read())
+        
+        #print orbPage.prettify()
+        
+        # Loop through inner directory
+        for line in orbPage.findAll('a'):
+            pspName = line.string
+            if not 'PSP' in pspName: # Skip link up a directory
+                continue
+            
+            ## No need to loop through the next level; the file names are fixed.
+            #pspPath = orbPath + pspName + pspName[:-1]
+            
+            # TODO: Don't store paths, just store the name.
+            #outputFile.write(pspPath + '\n')
+            outputFile.write(pspName[:-1] + '\n')
+            
+    outputFile.close()
+    
+    print 'Wrote updated data list to ' + outputFilePath
+
+
+def uploadFile(filePrefix, remoteFilePath, logQueue, tempDir):
+    """Uploads a remote file to maps engine"""
+    
+    print 'Uploading file ' + remoteFilePath
+    
+    
+    localFilePath = os.path.join(tempDir, os.path.basename(remoteFilePath))
+    if not os.path.exists(localFilePath):
+        # Download the file
+        cmd = 'wget ' + remoteFilePath + ' -O ' + localFilePath
+        print cmd
+        os.system(cmd)
+    
+    # Upload the file
+    cmdArgs = [localFilePath]
+    #print cmdArgs
+    assetId = GoogleMapsEngine.main(cmdArgs)
+    #assetId = 12345
+    
+    #TODO: Check to make sure the file made it up!
+    
+    # Record that we uploaded the file
+    logString = filePrefix + ', ' + str(assetId) + '\n' # Log path and the Maps Engine asset ID
+    #print logString
+    logQueue.put(logString)
+
+        
+    # Delete the file
+    print 'rm ' + localFilePath
+    #os.remove(localFilePath)
+    
+    print 'Finished uploading HiRISE data file'
+    return assetId
+
+
+def logWriter(logQueue, logPath):
+    '''listens for messages on the q, writes to file. '''
+
+    print 'Starting writer'
+    f = open(logPath, 'a') 
+    while 1: # Run until the process is killed
+        message = logQueue.get() # Wait for a new message
+        if message == 'stop_queue': # Check for the quit signal
+            break
+        f.write(str(message))
+        f.flush()
+    f.close()
+    print 'Writer stopped'
+
+
+# TODO: Select from different file types
+def uploadNextFile(dataListPath, outputFolder, getColor=False, numFiles=1, numThreads=1):
+    """Determines the next file to upload, uploads it, and logs it"""
+    
+    print 'Searching for next file to upload...'
+    
+    # Set up the output paths    
+    #uploadedColorPath   = os.path.join(outputFolder, 'uploadedPatchFilesTest.txt')
+    uploadedRedPath   = os.path.join(outputFolder, 'uploadedRed.csv')
+    uploadedColorPath = os.path.join(outputFolder, 'uploadedColor.csv')
+    
+    logPath   = uploadedRedPath
+    targetEnd = '_RED.JP2'
+    if getColor:
+        logPath   = uploadedColorPath
+        targetEnd = '_COLOR.JP2'
+    
+    inFile = open(dataListPath,    'r')
+
+    # Get the last line read (could be more efficient)
+    lastUploadedLine = ''
+    if os.path.exists(logPath):
+        outFile = open(logPath, 'r')
+        for line in outFile:
+            lastUploadedLine = line
+        outFile.close()
+    lastUploadedLine = lastUploadedLine.split(',')[0].strip() # Remove the asset ID from the string
+    #print '#' + lastUploadedLine + '#'
+    
+    # Now find that line in the input file list
+    linesToProcess = []
+    breakNext = -1
+    for line in inFile:
+        line = line.strip() # Remove \n from the line
+        #print '#' + line + '#'
+        
+        if breakNext > 0:
+            linesToProcess.append(line) # Record this line and move on to the next
+            breakNext = breakNext - 1
+            continue
+        if breakNext == 0: # No more lines to get!
+            break
+
+        if (lastUploadedLine == ''): # This is the first file!
+            linesToProcess.append(line)
+            breakNext = numFiles - 1 # Still get the next N-1 files
+            continue
+        if (line == lastUploadedLine): # Found the last one downloaded
+            breakNext = numFiles # Get the next N-1 files
+            continue
+    
+    if linesToProcess == []: # Make sure we found the next lines
+        raise Exception('Failed to find line that comes after: ' + lastUploadedLine)
+    
+    # Create processing pool
+    print 'Spawning ' + str(numThreads) + ' worker threads'
+    pool = multiprocessing.Pool(processes=numThreads+1) # One extra thread for the logWriter
+
+    # Create multiprocessing manager and a queue
+    manager = multiprocessing.Manager()
+    queue   = manager.Queue()    
+
+    # Start up the log writing thread
+    print 'Writing output to file ' + logPath
+    logResult = pool.apply_async(logWriter, args=(queue, logPath))
+    
+    # For each line generate the full download path
+    jobResults = []
+    for line in linesToProcess:
+        fullPath = generatePdsPath(line, targetEnd)
+        jobResults.append(pool.apply_async(uploadFile, args=(line, fullPath, queue, outputFolder)))
+    
+    
+    # Wait until all threads have finished
+    print 'Waiting for all threads to complete...'
+    for r in jobResults:
+        r.get()
+    
+    # Stop the queue and all the threads
+    print 'Cleaning up...'
+    queue.put('stop_queue')
+    pool.close()
+    pool.join()
+    
+    print 'All threads finished!'
+   
+    
+    return True
+    
+
+
+
+
+
+#--------------------------------------------------------------------------------
+
+def main():
+
+    print "Started hiriseDataLoader.py"
+
+    try:
+        try:
+            usage = "usage: hiriseDataLoader.py <output folder> [--help][--manual]\n  "
+            parser = optparse.OptionParser(usage=usage)
+            parser.add_option("-u", "--upload", dest="upload", type=int,
+                              help="Upload this many files instead of fetching the list.")
+            #parser.add_option("-o", "--output-folder", dest="outputFolder",
+            #                  help="Specifies the folder to copy the data to.",
+            #                  default='./')
+            #parser.add_option("-n", "--name", dest="name",
+            #                  help="Only get the data for the DTM with this name.",
+            #                  default='')
+            
+            parser.add_option("--color", action="store_true", dest="getColor",
+                              help="Retrieve COLOR image instead of RED images.")
+
+            parser.add_option("--threads", type="int", dest="numThreads", default=1,
+                              help="Number of threads to use.")
+                              
+            parser.add_option("--manual", action="callback", callback=man,
+                              help="Read the manual.")
+            (options, args) = parser.parse_args()
+
+            if len(args) < 1:
+                raise Exception('Missing output folder!')
+                return 1;
+            options.outputFolder = args[0]
+
+
+        except optparse.OptionError, msg:
+            raise Usage(msg)
+
+        print "Beginning processing....."
+
+        startTime = time.time()
+
+        if not os.path.exists(options.outputFolder):
+            os.mkdir(options.outputFolder)
+
+        # These are input lists that just show the available images
+        pspListPath = os.path.join(options.outputFolder, 'pspList.csv')
+        espListPath = os.path.join(options.outputFolder, 'espList.csv')
+        fullListFile = os.path.join(options.outputFolder, 'fullList.csv')
+
+        # If we are not uploading data, update the data list
+        if not options.upload:
+            getDataList(pspListPath, 'PSP')
+            getDataList(espListPath, 'ESP')
+            # TODO: Concatenate files!!!
+            #cmd = 'cat'
+            #os.system(cmd)
+        else:
+            uploadNextFile('hiriseImageList_smallPatch.txt', options.outputFolder, options.getColor, options.upload, options.numThreads)
+            #uploadNextFile(fullListFile, options.getColor, options.upload, options.numThreads)
+
+
+        endTime = time.time()
+
+        print "Finished in " + str(endTime - startTime) + " seconds."
+        return 0
+
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        return 2
+
+	# To more easily debug this program, comment out this catch block.
+    # except Exception, err:
+    #     sys.stderr.write( str(err) + '\n' )
+    #     return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ mapsEngineUpload.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __BEGIN_LICENSE__
+#  Copyright (c) 2009-2013, United States Government as represented by the
+#  Administrator of the National Aeronautics and Space Administration. All
+#  rights reserved.
+#
+#  The NGT platform is licensed under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance with the
+#  License. You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# __END_LICENSE__
+
+import sys, os, glob, optparse, re, shutil, subprocess, string, time
+
+import json, urllib2, requests, argparse
+
+import apiclient
+from apiclient import discovery
+import httplib2
+from oauth2client import client
+from oauth2client import file as oauth2client_file
+from oauth2client import tools
+
+# Authorization codes
+# TODO: Load these!
+
+def man(option, opt, value, parser):
+    print >>sys.stderr, parser.usage
+    print >>sys.stderr, '''\
+Tool for uploading raster images to Google Maps Engine
+'''
+    sys.exit()
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+# TODO: Is there a way to check for a file without knowing the asset ID?
+def checkIfFileIsLoaded(bearerToken, assetId = '04070367133797133737-15079892155897256865'):
+    """Determine if a file has already been uploaded into Maps Engine"""
+
+    # Send request for information on this asset
+    url = 'https://www.googleapis.com/mapsengine/v1/rasters/' + assetId
+    tokenString = 'Bearer '+bearerToken
+    headers = {'Authorization': tokenString}
+    response = requests.get(url, headers=headers)
+    
+    # See if we got a response
+    if response.status_code != 200:
+        return (False, response.status_code)
+    
+    # TODO: More accurate check?
+    # Check if all files are loaded and processing is finished    
+    jsonDict = json.loads(response.text)
+    status = True
+    for f in jsonDict['files']:
+        if (f['uploadStatus'] != 'complete'):
+            status = False
+    if jsonDict['processingStatus'] != 'complete':
+        status = False
+
+    print response.text
+    return (status, response.status_code)
+    
+    #url = 'https://www.googleapis.com/mapsengine/v1/assets?projectId='+PROJECT_ID
+    #tokenString = 'Bearer '+bearerToken
+    #headers = {'Authorization': tokenString
+    #response = requests.get(url, headers=headers)
+    #print response.text
+    # ?
+    # raster collection ID = GET /rasterCollections?projectId=<id>
+    #   raster list = GET /rasterCollections/#rasterCollectionId$/rasters
+    
+#--------------------------------------------------------------------------------
+
+# TODO: Need to update these to use the refresh token for "offline" access!
+
+def getCredentials(redo=False):
+
+    # Parse OAuth command line arguments
+    parser = argparse.ArgumentParser(parents=[tools.argparser])
+    #flags = parser.parse_args()
+    flags, unknown = parser.parse_known_args()
+    
+    print 'Looking for cached credentials'
+    # Check if we already have a file with OAuth credentials
+    storage = oauth2client_file.Storage('mapsengine.dat')
+    credentials = storage.get()
+    
+    
+    if credentials is None or credentials.invalid or redo:
+        print 'Getting credentials'
+        # Start local server, redirect user to authentication page, receive OAuth
+        # credentials on the local server, and store credentials in file
+        flow = client.OAuth2WebServerFlow(
+                            client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            response_type='code',
+                            scope='https://www.googleapis.com/auth/mapsengine',
+                            user_agent='Google-MapsEngineApiSample/1.0',
+                            redirect_uri='urn:ietf:wg:oauth:2.0:oob',
+                            access_type='offline',
+                            approval_prompt='auto') # Change to 'auto'
+        credentials = tools.run_flow(flow, storage, flags)
+      
+    return credentials
+
+def authorize(redo=False):
+
+    credentials = getCredentials(redo)    
+    
+    #jsonCredentials = credentials.to_json()
+    #jsonDict = json.loads(jsonCredentials)
+    #for key, value in jsonDict.iteritems():
+    #    print key + ' ---> ' + str(value)
+    
+    # Set up discovery with authorized credentials
+    http = credentials.authorize(httplib2.Http())
+    
+    service = apiclient.discovery.build('mapsengine', 'v1', http=http, developerKey=API_KEY)
+
+    # It is not clear why but adding this pointless code fixed the authorization problems!
+    #print '=================='    
+    # Read the location of every Feature in a Table.
+    features = service.tables().features()
+    request = features.list(id='12421761926155747447-06672618218968397709',
+                            maxResults=10, version='published')
+    response = request.execute()
+    #for feature in response['features']:
+    #  print feature['geometry']['coordinates']
+    #print '---------------------'
+
+    jsonCredentials = credentials.to_json()
+    jsonDict = json.loads(jsonCredentials)
+    #for key, value in jsonDict.iteritems():
+    #    print key + ' ---> ' + str(value)
+    
+    bearerToken = jsonDict['access_token']
+    #print bearerToken
+    return bearerToken
+    
+
+## Read the location of every Feature in draft version of Table.
+#features = service.tables().features()
+#request = features.list(id=TABLE_ID)
+#while request is not None:
+#  resource = request.execute()
+#  for feature in resource['features']:
+#    print feature['geometry']['coordinates']
+#
+#  # Is there an additional page of features to load?
+#  request = features.list_next(request, resource)
+
+def createRasterAsset(bearerToken, inputFile):
+    
+    url = 'https://www.googleapis.com/mapsengine/v1/rasters/upload'
+    
+    justFilename = os.path.basename(inputFile)
+    data = ( 
+    {
+      "projectId": PROJECT_ID,  # REQUIRED, taken from Maps Engine URL
+      "name": 'HiRISE_'+justFilename,  # REQUIRED
+      "description": "HiRISE map projected RDR data",
+      "files": [ # REQUIRED
+        { "filename": justFilename }
+      ],
+      #"acquisitionTime": {
+      #  "start": "2010-01-01T12:00:00Z",
+      #  "end": "2010-12-01T12:00:00Z",
+      #  "precision": "second"
+      #},
+      "draftAccessList": "Map Editors", # REQUIRED
+      "attribution": "NASA Public Domain", # REQUIRED
+      "tags": ["Mars", "MRO", "HiRISE"],
+      "maskType": "autoMask"
+    } )
+    print(data)
+    tokenString = 'Bearer '+bearerToken
+    headers = {'Authorization': tokenString,
+               'Content-Type': 'application/json'}
+    print(headers)
+    
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    
+    print response.text
+    
+    print('Received status code ' + str(response.status_code))
+    if response.status_code == 401:
+        print('Error: Unauthorized access!')
+    if response.status_code == 403:
+        print('Error: Forbidden operation!')
+    if response.status_code != 200:
+        return (False, response.status_code)
+    
+    jsonDict = json.loads(response.text)
+    #for key, value in jsonDict.iteritems():
+    #    print key + ' ---> ' + str(value)
+       
+    # Return the asset ID from the response
+    return (True, jsonDict['id'])
+    
+
+
+def uploadFile(bearerToken, assetId, filename):
+
+    # Check input image
+    if not os.path.exists(filename):
+        raise Exception('Input image file ' + filename + ' is missing!')
+    imageSizeBytes = os.path.getsize(filename)
+    
+    # Get the file extension tag to use
+    ext = os.path.splitext(filename)[1]
+    if (ext.lower == '.jp2'):
+        contentString = 'image/jpg2'
+    else: # Default to geotiff type
+        contentString = 'image/tiff'
+    
+    # Set up POST request
+    justFilename = os.path.basename(filename)
+    url = 'https://www.googleapis.com/upload/mapsengine/v1/rasters/'+str(assetId)+'/files?filename='+justFilename
+    tokenString = 'Bearer '+bearerToken
+    headers = {'Authorization':  tokenString,
+               'Content-Type':   contentString,
+               'Content-Length': str(imageSizeBytes)}
+
+    print headers
+    print url
+    print filename
+
+    # Submit the post request with file data
+#    fileList = {'file': open(filename, 'rb')}
+#    response = requests.post(url, headers=headers, files=fileList)
+ 
+    with open(filename, 'rb') as f:
+        response = requests.post(url, headers=headers, data=f) 
+    
+    print response.text
+    
+    # Check response status code
+    print('Received status code ' + str(response.status_code))
+    if response.status_code != 204:
+        print 'Failed to upload file!'
+        return False
+    print 'File upload started successfully!'
+    return True
+ 
+
+    # To check upload progress:
+    # GET https://www.googleapis.com/mapsengine/v1/rasters/{raster_ID}
+    # Authorization: Bearer {token}
+
+
+def main(argsIn):
+
+    print ('#################################################################################')
+    print ("Running mapsEngineUpload.py")
+
+    #try:
+    #try:
+    usage = "usage: mapsEngineUpload.py <input image> [--manual]\n  "
+    parser = optparse.OptionParser(usage=usage)
+
+    parser.add_option("--manual", action="callback", callback=man,
+                      help="Read the manual.")
+    (options, args) = parser.parse_args(argsIn)
+
+    if len(args) < 1: # DEBUG
+        #options.inputPath = 'means.png'
+        options.inputPath = '/home/smcmich1/data/production/NAC_DTM_M151318807_M181974094/results/output-DEM.tif'
+        #options.inputPath = '/home/smcmich1/data/google/mapsengine-cmd-line-sample/PSP_001427_1820_RED.JP2'
+    else:
+        options.inputPath = args[0]
+    
+    #except(optparse.OptionError, msg):
+    #    raise Usage(msg)
+    
+    
+    if not os.path.exists(options.inputPath):
+        raise Exception('Input file does not exist!')
+
+    startTime = time.time()
+
+    # Get server authorization
+    bearerToken = authorize()
+    
+    
+    #checkIfFileIsLoaded(bearerToken)
+    #raise Exception('----TEST----')
+    
+    
+    # Create empty raster asset request
+    (success, assetId) = createRasterAsset(bearerToken, options.inputPath)
+    #if not success:
+    #    print 'Refreshing access token...'
+    #    bearerToken = authorize(True)
+    #    (success, assetId) = createRasterAsset(bearerToken, options.inputPath)
+    if not success:
+        raise Exception('Could not get access token!')
+    
+    if success:
+        print 'Created asset ID ' + str(assetId)
+    
+        # Load a file associated with the asset
+        uploadFile(bearerToken, assetId, options.inputPath)
+
+    
+    endTime = time.time()
+
+    print("Finished in " + str(endTime - startTime) + " seconds.")
+    print('#################################################################################')
+    return assetId
+
+    #except(Usage, err):
+    #    print(err)
+    #    #print(>>sys.stderr, err.msg)
+    #    return 2
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
